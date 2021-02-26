ae92d44ce6267e454ffcf32a252661186a64f30c
JossueLarrea <75407040+JossueLarrea@users.noreply.github.com>
2021-02-25 20:09:33 -0500

    Android

---
+++ Quinto20202021/app/src/main/java/com/example/quinto2020_2021/ActivityNombres.java
@@ -0,0 +1,21 @@
+package com.example.quinto2020_2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ActivityNombres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nombres);
+        TextView textoNombres = findViewById(R.id.lblNombres);
+        TextView textoApellidos = findViewById(R.id.lblApellidos);
+        Bundle bundle = this.getIntent().getExtras();
+        textoNombres.setText(bundle.getString("nombre"));
+        textoApellidos.setText(bundle.getString("apellido"));
+    }
+}
